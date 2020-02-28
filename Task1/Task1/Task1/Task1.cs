5dc13a23875bad9f1b3d6552ad8570e365a4d50f
Nikola Žitko <nikola.zitko@gmail.com>
2020-02-28 18:08:36 +0100

    update

--- Task1/Task1/Task1/Task1.cs
+++ Task1/Task1/Task1/Task1.cs
@@ -11,54 +11,36 @@ namespace Task1
     {
         static void Main(string[] args) {
             //Currency,  Integer,  Scientific, Fixed-point, General, Number, Hexadecimal
-            string num1, num2;
-            int number1, number2;
-            double rez;
-
+            
             Console.WriteLine("Unesi prvi broj: ");
-            num1 = Console.ReadLine();
+            string num1 = Console.ReadLine();
             Console.WriteLine("Unesi drugi broj: ");
-            num2 = Console.ReadLine();
+            string num2 = Console.ReadLine();
 
             try {
-                number1 = Int32.Parse(num1);
-                number2 = Int32.Parse(num2);
-                rez = (double)number1 / (double)number2;
+                int number1 = Int32.Parse(num1);
+                int number2 = Int32.Parse(num2);
+                double rez = number1 / (double)number2;
 
+                //currency
                 Console.WriteLine(rez.ToString("C", CultureInfo.CurrentCulture));
-
-                try {
-                    int num;
-                    num = (int)rez;
-                    Console.WriteLine(num);
-                }
-                catch (FormatException) {
-                    Console.WriteLine($"Unable to parse INTEGER!");
-                }
-
+                //integer
+                Console.WriteLine(Convert.ToInt32(rez));
+                //scientific
                 Console.WriteLine(rez.ToString("E"));
-
+                //fixed point
                 Console.WriteLine(rez.ToString("F"));
-
+                //general
                 Console.WriteLine(rez.ToString("G"));
-
+                //number
                 Console.WriteLine(rez.ToString("N"));
-
-                try {
-                    int num;
-                    num = (int)rez;
-                    Console.WriteLine(num.ToString("x"));
-                }
-                catch (FormatException) {
-                    Console.WriteLine($"Unable to parse HEXADECIMAL!");
-                }
+                //hexadecimal
+                Console.WriteLine(Convert.ToInt32(rez).ToString("X"));
 
             }
-            catch (FormatException) {
-                Console.WriteLine($"Unable to parse");
+            catch (Exception e){
+                Console.WriteLine("Unable to parse, exception: " + e);
             }
-
-            Console.ReadLine();
 
         }
     }
